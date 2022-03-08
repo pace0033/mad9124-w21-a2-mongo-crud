49282693927c7ccb151b586bb62ac48055da20d6
Will Paceley <pace0033@algonquinlive.com>
2022-03-08 15:09:49 -0500

    add Student model

---
+++ models/Student.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.schema({
+  firstName: { type: String, maxlength: 64, required: true },
+  lastName: { type: String, maxlength: 64, required: true },
+  nickName: { type: String, maxlength: 64 },
+  email: { type: String, maxlength: 512, required: true },
+});
+
+const Model = mongoose.model('Student', schema);
+
+export default Model;
