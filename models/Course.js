e4aadc8ead88c81565c0db9c1e91bf773a68ee29
Will Paceley <pace0033@algonquinlive.com>
2022-03-08 16:45:42 -0500

    add Course model

---
+++ models/Course.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  code: { type: String, maxlength: 16, required: true },
+  title: { type: String, maxlength: 255, required: true },
+  description: { type: String, maxlength: 2048 },
+  url: { type: String, maxlength: 512 },
+  students: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Student'}
+  ]
+});
+
+const Model = mongoose.model('Course', schema);
+
+export default Model;
